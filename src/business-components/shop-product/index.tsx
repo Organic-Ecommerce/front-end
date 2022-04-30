c230b88811e8b18b4540ab7af3f03323001d694c
Scarlate <scarlate100@gmail.com>
2022-04-30 14:20:22 -0300

    chore: update page shop

---
+++ src/business-components/shop-product/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { Box, Flex } from 'rebass'
+import styled from 'styled-components'
+import { Header } from '../../business-components/header'
+import { CardLink } from '../../components/card/card-link'
+import { Title } from '../../components/title'
+
+import Delete from '../../assets/images/Delete.png'
+
+import { ShopGroup, CartStyles } from './styles'
+
+export interface CardCartProps {
+  id?: number
+  title?: string
+  price?: number
+  username?: string
+  photo?: string
+  unids?: any
+}
+
+export const ShopProduct = ({ title, price, username, photo, unids }: CardCartProps ) => {
+
+  const [unid, setUnid] = useState<number>(unids)
+  
+   async function adiciona (unid: number){
+    setUnid(unid + 1)
+   }; 
+
+   async function subtrai (unid: number){
+     return  unid >= 1 ? setUnid(unid - 1) : ""
+   }; 
+
+   async function remove (unid: number){
+    setUnid(unid = 0)
+   };
+
+  return (
+    <>
+    <ShopGroup>
+      <Flex alignItems='center'>
+        <CardLink
+          photo={photo}
+          title={title}
+          price={price }
+          username={username}
+        />
+        <Flex width='130px' justifyContent='space-between'> 
+        <CartStyles onClick={() => subtrai(unid)}> - </CartStyles>
+          <span>{unid}</span>
+          <CartStyles onClick={() => adiciona(unid)}> + </CartStyles>
+          <CartStyles onClick={() => remove(unid)}>
+             <img src={Delete} alt="delete" />
+          </CartStyles>
+          </Flex>
+        </Flex>
+    </ShopGroup>
+    </>
+  )
+}
---
+++ src/components/card/card-geral/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const CardStyles = styled.div`
+width: 539px;
+height: 254px;
+box-shadow: 0px 5px 14px 3px rgba(0, 0, 0, 0.2);
+background: #FFFFFF;
+border: 0.5px solid #AEAEAE;
+box-sizing: border-box;
+margin: 2rem;
+display: flex;
+justify-content: space-around;
+align-items: center;
+flex-direction: column;
+`
