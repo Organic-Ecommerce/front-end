f86e771e6409b0a725ab684335deb5a051426f78
Scarlate <scarlate100@gmail.com>
2022-04-30 01:56:35 -0300

    feat: add page home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,31 +1,18 @@
 import React from 'react'
-import { Box, Flex } from 'rebass'
-import { ScrollMenu } from '../../business-components/scroll-menu'
-import { Input } from '../../components/input'
-import { NavBar } from '../../components/nav-bar'
-import { InputRadio } from '../../components/radio'
-import { RadioMenu } from '../../components/radio/radio-menu'
-import { Title } from '../../components/title'
+
+import { Header } from '../../business-components/header'
+import { SalesMenu } from '../../business-components/sales-menu'
+import { FilterList } from '../../business-components/filter'
+
+import { array, product } from './mock'
 
 export const Home = () => {
+
   return(
     <>
-      <NavBar>
-        <Input placeholder='Procurar'/>
-      </NavBar>
-        <Box ml='6rem' mb='2rem'>
-          <Title>Horta Orgânic</Title>
-        </Box>
-        <ScrollMenu/>
-        <Flex flexDirection='column' alignItems='center'>
-          <Title>Nossa Horta</Title>
-          <Flex width='100%' justifyContent='space-around'>
-          <RadioMenu text='teste' label='label'/>
-          <RadioMenu text='teste' label='label'/>
-          <RadioMenu text='teste' label='label'/>
-        </Flex>
-        </Flex>
-       
+    <Header/>
+    <FilterList array={array} />
+    <SalesMenu array={product} />
    </>
   )
 }
