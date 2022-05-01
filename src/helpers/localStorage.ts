f8a99e7b504d648f24c684f3ba8e8ccf441b4123
raniererocha <rlsmont123@hotmail.com>
2022-05-01 12:02:11 -0300

    feat: add tools for use localStorage

---
+++ src/helpers/localStorage.ts
@@ -0,0 +1,9 @@
+export const getItem = (key: string) => {
+  const resp = window.localStorage.getItem(key) ?? "{}"
+  return JSON.parse(resp)
+}
+export const setItem = (key: string, value: string | object) =>
+  window.localStorage.setItem(key, JSON.stringify(value))
+
+export const removeItem = (key: string) => window.localStorage.removeItem(key)
+export const clear = () => window.localStorage.clear()
