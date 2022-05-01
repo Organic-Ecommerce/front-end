366f7041648bfc63acb9260ea65b24c4bfbf6909
raniererocha <rlsmont123@hotmail.com>
2022-05-01 12:00:39 -0300

    feat: Cart count bagde

--- src/business-components/header/index.tsx
+++ src/business-components/header/index.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react"
 
 import { Text, Flex } from "rebass"
@@ -11,7 +10,8 @@ import Carrinho from '../../assets/images/Carrinho.png'
 import Logo from '../../assets/images/Logo.png'
 import test from '../../assets/images/test.png'
 
-import { BoxStyles, CartStyles, PhotoStyles, AutocompleteStyles } from "./styles"
+import { BoxStyles, CartStyles, PhotoStyles, AutocompleteStyles, CartCount } from "./styles"
+import { getItem } from "../../helpers/localStorage";
 
 interface PhotoMenuProps {
   src?: string
@@ -19,77 +19,91 @@ interface PhotoMenuProps {
 
 export const Header = () => {
 
-  const test = [ {name: 'vanessa'}]
+  const test = [{ name: 'vanessa' }]
 
   const [element, setElement] = useState()
-  const [ filter, setFilter] =  useState<any>(null)
+  const [filter, setFilter] = useState<any>(null)
   const [display, setDisplay] = useState<string>('none')
+  const [cartCount, setCartCount] = useState<number>(0)
 
   async function fetchMyAPI(element: any) {
     let response = await fetch(`https://organicecommerce.herokuapp.com`)
-     response = await response.json()
-     setFilter(response)
-     // {JSON.stringify(filter)}
+    response = await response.json()
+    setFilter(response)
+    // {JSON.stringify(filter)}
   }
 
-console.log(filter)
- useEffect(() => {
-    if(element){
+  console.log(filter)
+  useEffect(() => { 
+    const cartList = getItem('cartList')
+    console.log(cartList)
+    cartList ? setCartCount(cartList.length) : setCartCount(0)
+  }, [])
+
+  useEffect(() => {
+    if (element) {
       fetchMyAPI(element)
     }
   }, [element])
 
-  return(
-    <div onClick={() => setDisplay('none')}> 
-    <NavBar>
-      <Menu/>
-      <BoxStyles >
-      <Input
-        onKeyUp={() => setDisplay('')} 
-        value={element} 
-        onChange={(event: any) => setElement(event.target.value)} 
-        width="100%" 
-        border 
-        placeholder="Procurar"
-      />
-      <AutocompleteStyles display={display}>
-     <Flex flexDirection='column' m='1rem' >
-    {test.map((name)=> (
-      <Text fontSize='18px' mb='1rem'>{name.name}</Text>
-    ))}
-     </Flex>
-      </AutocompleteStyles>
-      </BoxStyles>
-      <Flex style={{ gap: '4rem'}}>
-        <Link to='/shop'><Cart/></Link>
-        <PhotoMenu/>
-      </Flex>
+
+  return (
+    <div onClick={() => setDisplay('none')}>
+      <NavBar>
+        <Menu />
+        <BoxStyles >
+          <Input
+            onKeyUp={() => setDisplay('')}
+            value={element}
+            onChange={(event: any) => setElement(event.target.value)}
+            width="100%"
+            border
+            placeholder="Procurar"
+          />
+          <AutocompleteStyles display={display}>
+            <Flex flexDirection='column' m='1rem' >
+              {test.map((name) => (
+                <Text fontSize='18px' mb='1rem'>{name.name}</Text>
+              ))}
+            </Flex>
+          </AutocompleteStyles>
+        </BoxStyles>
+        <Flex style={{ gap: '4rem' }}>
+          <Link to='/shop'><Cart  cartItemsCount={cartCount}/></Link>
+          <PhotoMenu />
+        </Flex>
       </NavBar>
-      </div>
+    </div>
   )
 }
 
-const Cart = () => {
-  return(
+interface CartProps {
+  cartItemsCount: number
+}
+const Cart = ({cartItemsCount}: CartProps) => {
+  return (
     <CartStyles>
       <img src={Carrinho} />
-      </CartStyles>
+      {
+        cartItemsCount > 0 && <CartCount>{cartItemsCount > 9 ? "+9" : cartItemsCount}</CartCount>
+      }
+    </CartStyles>
   )
 }
 
 const PhotoMenu = ({ src }: PhotoMenuProps) => {
-  return(
+  return (
     <CartStyles>
-    <PhotoStyles src={src ? src : test} />
+      <PhotoStyles src={src ? src : test} />
     </CartStyles>
   )
 
 }
 
 const Menu = () => {
-  return(
+  return (
     <CartStyles>
-    <img src={Logo} />
+      <img src={Logo} />
     </CartStyles>
   )
 
--- src/business-components/header/styles.ts
+++ src/business-components/header/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components"
 export const CartStyles = styled.button`
 background-color: transparent;
 border: none;
+position: relative;
 cursor: pointer;
 `
 
@@ -28,4 +29,28 @@ background: pink;
 position: relative;
 background: red;
 width: 40%;
+`
+
+export const CartCount = styled.span`
+
+  width: 1.2rem;
+  height: 1.2rem;
+
+  padding: .2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: absolute;
+  bottom: 0;
+  right: -5px;
+
+  background-color: red;
+
+  border-radius: 50%;
+  color: white;
+
+  font-size: .9rem;
+  font-weight: bold;
 `
